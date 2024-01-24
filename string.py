1b37dafe70aa83ea0174d37e821a6cb7d5369faa
Cameras <jmccruz1224@gmail.com>
2024-01-24 13:17:32 -0500

    working on strings

---
+++ string.py
@@ -0,0 +1,35 @@
+'''
+# print("Hello World")
+# print(help('keywords'))
+# a = b = c = 10
+# print(a,b,c)
+# x = 47  #statement
+# y = 47 + 10
+'''
+# print(int(10.24))
+# print(int("10.24"))
+# print(int(float("10.24")))
+# print(str(20))
+# print(type(str(20)))
+# print(list('hello'))
+# first_name = ('jane')
+# print(first_name)
+# last_name = "doe"
+# print(last_name)
+# address = '''10 Main St'''
+# print(address)
+# # job1 = "physician's assistant"
+# emp_name = "jane Doe"
+# print(len(emp_name))
+# print(emp_name.upper())
+# print(emp_name.lower())
+# first_name = "jane"
+# last_name = "Doe"
+# print(first_name + " " + last_name)
+# age = 24
+# print(first_name + " " + last_name + " " + str(age))
+# print("hello " * 3)
+emp_name = "Jane doe"
+print(emp_name[3])
+# print(emp_name[8])
+print(emp_name.index("n"))
