42237930ea8f620691c832647fd25ab9a5019daa
Cameras <jmccruz1224@gmail.com>
2024-01-24 14:00:50 -0500

    Learning new strings

--- string.py
+++ string.py
@@ -30,7 +30,25 @@
 # age = 24
 # print(first_name + " " + last_name + " " + str(age))
 # print("hello " * 3)
-emp_name = "Jane doe"
-print(emp_name[3])
-# print(emp_name[8])
-print(emp_name.index("n"))
+# emp_name = "Jane doe"
+# print(emp_name[3])
+# # print(emp_name[8])
+# print(emp_name.index("n"))
+
+emp_name = "jane Doe"
+print(emp_name[2:6])
+print(emp_name[:4])
+print(emp_name[5:])
+print(emp_name[-4:-1])
+print(emp_name[1:6:2])#the third number allows you to count every other number because it is 2 so it prints every other from 1-6
+print(emp_name.count('e'))
+print(emp_name.find('Doe'))
+emp_name = emp_name.replace("jane", "john")
+print("oh" in emp_name)
+
+student_name = "alice"
+score = 87
+print(student_name + ": " + str(score))
+print("{}: {}".format(student_name, score))
+print(f'{student_name}: {score}')
+print(f'10 times 3 is {10*3}')
